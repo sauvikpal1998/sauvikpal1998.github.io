b8a80268d291e7acf807aecc2a6ab3bba53ddf6f
sauvikpal1998 <sauvikpal1998@gmail.com>
2017-08-25 22:42:04 +0530

    Add files via upload

---
+++ ead2017/contact.php
@@ -0,0 +1,118 @@
+<html>
+  <head>
+    
+    <meta charset="utf-8">
+    
+    <!--Mobile meta-->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"> 
+    
+    <!--Import Google Icon Font-->
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    
+    <link href="css/materialize.css" rel="stylesheet" type="text/css">
+    <link href="css/materialize.min.css" rel="stylesheet" type="text/css">
+    <script src="https://use.fontawesome.com/5a3b12e8d8.js"></script>
+    
+    <style>
+    
+      img
+      {
+        height: 200px;
+      }
+      
+      .fb:hover
+      {
+        color: #3b5998;
+      }
+      
+      .linkedin:hover
+      {
+        color: #007bb5;
+      }
+   
+      a.info:hover
+      {
+        color: #007bb5;
+      }
+      
+      @media screen and (min-width: 993px)
+      {
+        .main
+        {
+          padding: 0 4.3%;
+        }
+      }
+      
+      .main
+      {
+        margin : 0 10%;
+      }
+          
+    </style>
+    
+    <title>
+      Entrepreneurship Awareness Drive 2017
+    </title>  
+  </head>
+  
+  <body>
+    
+    <div class="row main">
+<!--        offset-s1 -->
+        <div class="col s12 m6 l3">
+          <div class="card">
+            <div class="card-image">
+              <img src="images/sm2k17/abhinav.jpg">
+            </div>
+            <div class="card-content">
+              <span class="card-title blue-text">Abhinav Bhatt</span>
+              <p>Senior Manager<br> Public Relations and EAD Co-ordinator</p>
+              <br>
+              <a href="mailto:abhinav.bhatt@ecell-iitkgp.org" class="grey-text info text-darken-1">abhinav.bhatt@ecell-iitkgp.org</a>
+              <p class="grey-text text-darken-1">+91 9820828622</p>
+            </div>
+            <div class="card-action">
+              <a href="#"><i class="fa fa-lg fa-facebook fa-fw fb"></i></a>
+              <a href="#"><i class="fa fa-lg fa-linkedin linkedin"></i></a>
+            </div>
+          </div>
+        </div>
+<!--        offset-m2 offset-l1 offset-s1 -->
+        <div class="col s12 m6 l3">
+          <div class="card">
+            <div class="card-image">
+              <img src="images/sm2k17/abhinav.jpg">
+            </div>
+            <div class="card-content">
+              <span class="card-title blue-text">Abhinav Bhatt</span>
+              <p>Senior Manager<br> Public Relations and EAD Co-ordinator</p>
+            </div>
+            <div class="card-action">
+              <a href="#"><i class="fa fa-lg fa-facebook fa-fw fb"></i></a>
+              <a href="#"><i class="fa fa-lg fa-linkedin linkedin"></i></a>
+            </div>
+          </div>
+        </div>
+<!--        offset-s1 offset-l1 -->
+        <div class="col s12 m6 l3">
+          <div class="card">
+            <div class="card-image">
+              <img src="images/sm2k17/abhinav.jpg">
+            </div>
+            <div class="card-content">
+              <span class="card-title blue-text">Abhinav Bhatt</span>
+              <p>Senior Manager<br> Public Relations and EAD Co-ordinator</p>
+            </div>
+            <div class="card-action">
+              <a href="#"><i class="fa fa-lg fa-facebook fa-fw fb"></i></a>
+              <a href="#"><i class="fa fa-lg fa-linkedin linkedin"></i></a>
+            </div>
+          </div>
+        </div>
+      </div>
+    
+    <script src="js/jquery.js" type="text/javascript"></script>
+    <script src="js/materialize.js" type="text/javascript"></script>
+    <script src="js/materialize.min.js" type="text/javascript"></script>
+  </body>
+</html>
